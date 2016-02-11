eef5da24a095eb21eb6007198003dd81b5572cdf
Stephen Igwue <olibenu@yahoo.com>
2016-02-11 20:35:21 +0100

    Input file structure

--- inputs.php
+++ inputs.php
@@ -21,18 +21,31 @@ class InputFile
         //warehouses
         $input['warehouses'] = 0; //2
         $input['warehouse_details'] = array(
-            'warehouse_coord' => array(
-                array(
-                    'row'=>0,
-                    'col'=>0
-                )
-            ),
-            'warehouse_product_count' => array() //size = $input['products']
+            array (
+                'warehouse_coord' => array(
+                    array(
+                        'row'=>0,
+                        'col'=>0
+                    )
+                ),
+                'warehouse_product_count' => array() //size = $input['products']
+            )
         }
 
         //orders
         $input['orders'] = 0;
-        $input['order'] = 0;
+        $input['order_details'] = array(
+            array (
+                'delivery_coord' => array(
+                    array(
+                        'row'=>0,
+                        'col'=>0
+                    )
+                ),
+                'item_count' => 0,
+                'product_types' = array() //size = item_count
+            )
+        }
     }
 
 }
